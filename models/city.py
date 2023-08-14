172182480b8884938e2624ed6c300ab2d02e1b06
Sharon <Sharonwanjiru392@gmail.com>
2023-08-14 11:52:33 -0700

    city.py

--- models/city.py
+++ models/city.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-from models.base_model import BaseModel
-
-
-class City(BaseModel):
-=======
-"""Define a City class."""
+"""Class City"""
 from models.base_model import BaseModel
 
 class City(BaseModel):
-    """Represent city.
-    Attributes:
-    state_id (str): The state id.
-    name (str): The name of the city."""
->>>>>>> 654eb7633356ce5e35eadba8f9604d08a91d85e4
+    """ city class that inherits BaseModel """
     state_id = ""
     name = ""
